d3dc47471dd4c26803647b286efcdc55ed766917
Matt Gibbs <mgibbs@slac.stanford.edu>
2022-07-19 07:40:07 -0700

    Fix import for python3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='slacepics',
-	version='1.0.1',
+	version='1.0.2',
 	packages=['slacepics'],
 	description='Module for various EPICS-related tools, such as converting EPICS time to UNIX time.',
 	author='Patrick Pascual',
